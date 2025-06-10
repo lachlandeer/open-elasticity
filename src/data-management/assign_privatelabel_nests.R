6ccc5a74dc6800b3fe8ce0cc6a378fbfcfa13491
Lachlan Deer <lachlan.deer@gmail.com>
2025-06-09 20:40:45 -0500

    nest creator also counts number of products in category to use as within nest instrument

--- src/data-management/assign_privatelabel_nests.R
+++ src/data-management/assign_privatelabel_nests.R
@@ -2,7 +2,8 @@
 #'
 #' contributors: @lachlandeer
 #'
-#' Assign nest classification (PrivateLabel vs NationalBrand) based on brand-level PL status
+#' Assign nest classification (PrivateLabel vs NationalBrand) based on brand-level PL status.
+#' Also computes number of within-nest competitors by country-week.
 
 # ---- Libraries ----
 library(optparse)
@@ -18,7 +19,7 @@ option_list = list(
   make_option(c("-o", "--out"),
               type = "character",
               default = "brand_nests.csv",
-              help = "Output CSV with brand nest classification [default = %default]",
+              help = "Output CSV with brand nest classification and nest size info [default = %default]",
               metavar = "character")
 )
 
@@ -32,16 +33,28 @@ if (is.null(opt$data)) {
 
 # ---- Load data ----
 message("📥 Loading data from: ", opt$data)
-df <- read_csv(opt$data)
+df <- read_csv(opt$data, show_col_types = FALSE)
 
-# ---- Compute brand nest classification ----
-brand_nests <- df %>%
-  distinct(country, brand, pl) %>%
+# ---- Assign brand nest labels ----
+df <- df %>%
   mutate(
     nest = if_else(pl, "PrivateLabel", "NationalBrand")
-  ) %>%
-  select(country, brand, nest)
+  )
+
+# ---- Compute within-nest brand counts per country-week ----
+nest_counts <- df %>%
+  distinct(country, year_week, brand, nest) %>%
+  group_by(country, year_week, nest) %>%
+  summarise(
+    n_brands_in_nest = n_distinct(brand),
+    .groups = "drop"
+  )
+
+# ---- Merge back into panel ----
+brand_nests <- df %>%
+  distinct(country, year_week, brand, nest) %>%
+  left_join(nest_counts, by = c("country", "year_week", "nest"))
 
 # ---- Save output ----
-message("💾 Saving brand nests to: ", opt$out)
+message("💾 Saving brand nest classifications with nest size to: ", opt$out)
 write_csv(brand_nests, opt$out)
